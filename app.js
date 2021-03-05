24c3a902ab6f08e0b7d6367ea684f85a614dcf1c
mfbernasol <mfbernasol@gmail.com>
2021-03-04 23:37:43 -0600

    fixing form to save to database

--- app.js
+++ app.js
@@ -11,23 +11,66 @@ app.set('view engine','ejs');
 // app.use(express.static('public'));
 
 // const URL = 'mongodb+srv://test-user:user1234@cluster0.jdj0v.mongodb.net/points'
-mongoose.connect('mongodb+srv://test-user:user1234@cluster0.jdj0v.mongodb.net/messages', { useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true });
-const messageSchema = {
-  name: String
+mongoose.connect('mongodb+srv://test-user:user1234@cluster0.jdj0v.mongodb.net/employee', { useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true });
+const employeeSchema = {
+  lastName: String,
+  firstName: String, 
+  email: String, 
+  workID: String, 
+  vaccinate: {
+    type: Boolean,
+  },
+  // dateOfVac: Date,
+  infected: {
+    type: Boolean,
+  },
+  // datePositiveTest: Date,
+  // dateNegativeTest: Date, 
+  quarantine: {
+    type: Boolean, 
+  },
+  // quarantineStartDate: {
+  //   type: Boolean, 
+  // }
 }
 
-const Item = mongoose.model("Item",messageSchema);
+const Employee = mongoose.model("Employee",employeeSchema);
 
 app.get('/',function(req,res){
     res.render('test');
 })
 app.post("/",function(req,res){
-  const message = req.body.newMessage;
-  console.log(message);
-  const item = new Item({
-    name: message
+  const lastName = req.body.lname; 
+  const firstName = req.body.fname;
+  const email = req.body.email;
+  const workID = req.body. workID;
+  const vacStatus = req.body.vacStatus;
+  const infected = req.body.infected;
+  const dateVac = req.body.dateVac;
+  const datePostTest = req.body.datePositiveTest;
+  // const dateNegativeTest = req.body.dateNegativeTest;
+  const inQuarantine = req.body.quaratine;
+  const quarDate = req.body.quarDate;
+
+  
+// console.log(dateVac);
+console.log(vacStatus);
+  const employee = new Employee({
+    lastName: lastName,
+    firstName: firstName,
+    email: email,
+    workID: workID,
+    vaccinate: vacStatus,
+    dateOfVac:dateVac,
+    infected: infected,
+    // datePositiveTest: datePostTest,
+    // dateNegativeTest: dateNegativeTest,
+    // quarantine: inQuarantine,
+    // quarantineStartDate: quarDate
+
+
   });
-  item.save(function(err){
+  employee.save(function(err){
     if(err){
       console.log("err");
     }else{
