56f69dd66443c81149a480d4f4c531ab1dcd0b56
NFickett <71030361+NFickett@users.noreply.github.com>
2021-03-18 21:23:56 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -1,20 +1,38 @@
-const express = require('express');
+var path = require('path')
+const express = require("express");
+const mongoose = require("mongoose");
+const cookieParser = require("cookie-parser");
+//router
+const authRoutes = require("./routes/authRoutes");
 const app = express();
+var port = process.env.PORT ||3000;
 
-
-app.set('view engine','ejs');
+//midleware
 app.use(express.static('public'));
 
 
-app.get('/',function(req,res){
-    res.render('index');
-})
 
-app.get('/test',function(reqm,res){
-  res.render("test");
-})
+//veiw engine
+app.set('view engine','ejs');
 
-const port = process.env.PORT || 3000
-app.listen(port, () => console.log(
-  `Express started on http://localhost:${port}; ` +
-  `press Ctrl-C to terminate.`));
+// database connection
+
+//const dbURI =
+//  "mongodb+srv://Nathan:TrlDs2mXMnPbjEAP@cluster0.a6xht.mongodb.net/node-auth";
+//  mongoose
+//  .connect(dbURI, {
+//    useNewUrlParser: true,
+//    useUnifiedTopology: true,
+//    useCreateIndex: true,
+//  })
+//  .then((result) => app.listen(port))
+//  .catch((err) => console.log(err));
+
+app.listen(port)
+
+//routes
+app.get("/", (req, res) => res.render("index"));
+app.get("/test", (req, res) => res.render("test"));
+app.get('/faq',(req,res) => res.render("faq"))
+app.get('/announcements',(req,res) => res.render("announcements"))
+app.use(authRoutes);
