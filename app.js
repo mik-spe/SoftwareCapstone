b57dd781390ab96d3625cdfaecc2e75e8d3443d6
Mikala <Mik@Mikalas-MacBook-Pro.local>
2021-03-30 22:54:14 -0500

    Started working on using fetch to get database data in app.js.

--- app.js
+++ app.js
@@ -36,3 +36,35 @@ app.get("/test", (req, res) => res.render("test"));
 app.get('/faq',(req,res) => res.render("faq"))
 app.get('/announcements',(req,res) => res.render("announcements"))
 app.use(authRoutes);
+
+
+// Database Data
+function createNode(element) {
+    return document.createElement(element);
+}
+
+function append(parent, el) {
+  return parent.appendChild(el);
+}
+
+const ul = document.getElementById('employees');
+const url = 'https://beakon-employee.herokuapp.com/employees';
+
+fetch('https://beakon-employee.herokuapp.com/employees')
+.then((resp) => resp.json())
+.then(function(data) {
+  let authors = data.results;
+  return authors.map(function(author) {
+    let li = createNode('li');
+    let img = createNode('img');
+    let span = createNode('span');
+    img.src = author.picture.medium;
+    span.innerHTML = `${author.name.first} ${author.name.last}`;
+    append(li, img);
+    append(li, span);
+    append(ul, li);
+  })
+})
+.catch(function(error) {
+  console.log(error);
+});
