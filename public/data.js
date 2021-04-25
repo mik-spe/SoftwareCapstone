4fdc706142a8ee3bb5dcae08c7c60c5ca774a179
mik-spe <57027766+mik-spe@users.noreply.github.com>
2021-04-25 17:47:57 -0500

    edited data file for all fields on main table

--- public/data.js
+++ public/data.js
@@ -3,10 +3,10 @@ const api_url =
       "https://beakon-employee.herokuapp.com/employees";
   
 // Defining async function
-async function getapi(url) {
+async function getapi(api_url) {
     
     // Storing response
-    const response = await fetch(url);
+    const response = await fetch(api_url);
     
     // Storing data in form of JSON
     var data = await response.json();
@@ -27,20 +27,36 @@ function hideloader() {
 function show(data) {
     let tab = 
         `<tr>
-          <th>LastName</th>
-          <th>FirstName</th>
-          <th>Email</th>
-          <th>workID</th>
+            <th>Vaccinated</th>
+            <th>Infected</th>
+            <th>Quarantine</th>
+            <th>ID</th>
+            <th>Last Name</th>
+            <th>First Name</th>
+            <th>Email</th>
+            <th>work ID</th>
+            <th>Positive Test Date</th>
+            <th>Negative Test Date</th>
+            <th>Vaccine Date</th>
+            <th>Quarantine Date</th>
          </tr>`;
     
     // Loop to access all rows 
     for (let r of data) {
         tab += `<tr> 
-    <td>${r.lastName} </td>
-    <td>${r.firstName}</td>
-    <td>${r.email}</td> 
-    <td>${r.workID}</td>          
-    </tr>`;
+            <td>${r.vacStatus}</td> 
+            <td>${r.infected}</td>
+            <td>${r.inQuarantine} </td>
+            <td>${r._id}</td>
+            <td>${r.lastName}</td> 
+            <td>${r.firstName}</td>     
+            <td>${r.email}</td> 
+            <td>${r.workID}</td> 
+            <td>${r.datePositiveTest}</td> 
+            <td>${r.dateNegativeTest}</td>    
+            <td>${r.dateOfVac}</td>  
+            <td>${r.quarDate}</td> 
+        </tr>`;
     }
     // Setting innerHTML as tab variable
     document.getElementById("employees").innerHTML = tab;
